40cd7ea146e10704a2ea8d3a89fbddcec8cab531
Julio Hornos <jhornosb@gmail.com>
2025-02-15 21:02:22 +0100

    Commit de la rama Caso1

--- src/paquete/Programa.java
+++ src/paquete/Programa.java
@@ -9,6 +9,10 @@ public class Programa {
 		System.out.println("prueba de add+commit de GIT");
 		
 		System.out.println("prueba soft reset de GIT");
+		
+		RamaCaso1 miRamaCaso1 = new RamaCaso1();
+		
+		miRamaCaso1.pintarRama();
 
 	}
 
---
+++ src/paquete/RamaCaso1.java
@@ -0,0 +1,11 @@
+package paquete;
+
+public class RamaCaso1 {
+	
+	public void pintarRama() {
+		System.out.println("Estamos en la rama 1");
+	}
+
+}
+
+

c1873295d03d82c87062abc743247c3c6901faba
Julio Hornos <jhornosb@gmail.com>
2025-02-15 12:27:17 +0100

    Primer commit

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module PruebasLocales01 {
+}
---
+++ src/paquete/Programa.java
@@ -0,0 +1,11 @@
+package paquete;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("prueba de uso de GIT");
+
+	}
+
+}
